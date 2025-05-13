7d24dc7c23907ffae56f445633c4e91bec683aaa
fexburgos <fexburgos@gmail.com>
2025-05-13 09:18:10 -0500

    Class May12

---
+++ ESTUDIANTE/07_operadores.dart
@@ -0,0 +1,24 @@
+void main () {
+  final valor1= 1;
+  final valor2 = 2;
+
+  final suma = valor1 + valor2;
+  print (suma);
+
+  final (resta) = valor2 - valor1;
+  print (resta);
+
+  final (multiplicacion) = valor1 * valor2;
+  print (multiplicacion);
+
+  final (division) = valor1 / valor2;
+  print(division);
+
+
+
+
+
+
+
+
+}
